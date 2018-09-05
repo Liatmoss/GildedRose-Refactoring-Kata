2b7aed70df48cd590c3b96a8683869c6ff40ef6c
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-05 11:17:47 +0100

    Master: Added some tests for basic value degredation

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -4,10 +4,29 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
+    it('should contain the correct item', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
+        expect(items.length).to.equal(1);
         expect(items[0].name).to.equal('foo');
+    });
+
+    it('sellIn should decrease at end of day', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 10, 10)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(9);
+    });
+
+    it('quality decreases by 1 if sell by date has not expired', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 10, 10)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(9);
+    });
+
+    it('quality decreases by 2 if sell by date has expired', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 0, 10)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(8);
     });
 
 });
