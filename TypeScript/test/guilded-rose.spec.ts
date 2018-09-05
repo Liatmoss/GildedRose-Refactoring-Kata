5281d2b13ae10f3be22b188f0bc18af94e54bc9a
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-05 12:11:45 +0100

    Master: Refactored tests to reduce duplicate code.

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -2,11 +2,34 @@ import 'mocha';
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
+function testSellIn(testItem: Item, expectedSellIn: number) {
+    //Arrange
+    const gildedRose = new GildedRose([testItem]);
+    //Act
+    const items = gildedRose.updateQuality();
+    //Assert
+    expect(items[0].sellIn).to.equal(expectedSellIn);
+}
+
+function testQuality(testItem: Item, expectedQuality: number) {
+    //Arrange
+    const gildedRose = new GildedRose([testItem]);
+    //Act
+    const items = gildedRose.updateQuality();
+    //Assert
+    expect(items[0].quality).to.equal(expectedQuality);
+}
+
+function testBoth(testItem: Item, expectedSellIn: number, expectedQuality: number) {
+    testSellIn(testItem, expectedSellIn);
+    testQuality(testItem, expectedQuality);
+}
+
 describe('Gilded Rose', function () {
 
     describe('updateQuality()', function () {
 
-        describe('general item tests', function() {
+        describe('General item tests', function() {
 
             it('should contain the correct item', function() {
                 //Arrage
@@ -19,39 +42,19 @@ describe('Gilded Rose', function () {
             });
         
             it('sellIn should decrease at end of day', function() {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('foo', 10, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].sellIn).to.equal(9);
+                testSellIn(new Item('foo', 10, 10), 9);
             });
         
             it('quality decreases by 1 if sell by date has not expired', function() {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('foo', 10, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(9);
+                testQuality(new Item('foo', 10, 10), 9);
             });
         
             it('quality decreases by 2 if sell by date has expired', function() {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('foo', 0, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(8);
+                testQuality(new Item('foo', 0, 10), 8);
             });
         
             it('quality does not become negative', function() {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('foo', 10, 0)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(0);
+                testQuality(new Item('foo', 10, 0), 0);
             })
 
         })
@@ -59,21 +62,11 @@ describe('Gilded Rose', function () {
         describe('Aged Brie tests', function() {
 
             it('Aged Brie increases in quality the older it gets', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Aged Brie', 10, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(11);
+                testQuality(new Item('Aged Brie', 10, 10), 11);
             })
         
             it('quality cannot increase beyond 50', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Aged Brie', 10, 50)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(50);
+                testQuality(new Item('Aged Brie', 10, 50), 50);
             })
 
         })
@@ -81,13 +74,7 @@ describe('Gilded Rose', function () {
         describe('Legendary item tests', function () {
 
             it('Sulfuras never has to be sold or loses value', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 10, 25)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].sellIn).to.equal(10);
-                expect(items[0].quality).to.equal(25);
+                testBoth(new Item('Sulfuras, Hand of Ragnaros', 10, 25), 10, 25);
             })
 
         })
@@ -95,39 +82,19 @@ describe('Gilded Rose', function () {
         describe('Backstage passes tests', function () {
 
             it('Backstage Passes increase in quality when > 10 days left', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 20, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(11);
+                testQuality(new Item('Backstage passes to a TAFKAL80ETC concert', 20, 10), 11);
             })
         
             it('Backstage Passes increase in quality by 2 when <= 10 days left', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(12);
+                testQuality(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10), 12);
             })
         
             it('Backstage Passes increase in quality by 3 when <= 5 days left', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(13);
+                testQuality(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10), 13);
             })
         
             it('Backstage Passes become worthless after the concert', function () {
-                //Arrange
-                const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)]);
-                //Act
-                const items = gildedRose.updateQuality();
-                //Assert
-                expect(items[0].quality).to.equal(0);
+                testQuality(new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10), 0);
             })
 
         })
